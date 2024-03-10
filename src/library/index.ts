45d1ed6505c313d11d1b9783909cdef37705946c
vilicvane <vilicvane@live.com>
2024-03-10 23:56:50 +0800

    Initial implementation and tests

---
+++ src/library/index.ts
@@ -0,0 +1,53 @@
+import type {Observable, Observer, OperatorFunction} from 'rxjs';
+import {Subject} from 'rxjs';
+import {OperatorSubscriber} from 'rxjs/internal/operators/OperatorSubscriber';
+import {operate} from 'rxjs/internal/util/lift';
+
+export function branch<T>(
+  predicate: (value: T) => boolean,
+): OperatorFunction<T, Observable<T>> {
+  return operate((source, subscriber) => {
+    const branchSet = new Set<Subject<T>>();
+
+    const notify = (
+      callback: (branch: Observer<Observable<T>> | Observer<T>) => void,
+    ): void => {
+      for (const branch of branchSet) {
+        callback(branch);
+      }
+
+      callback(subscriber);
+    };
+
+    const handleError = (error: unknown): void =>
+      notify(consumer => consumer.error(error));
+
+    const branchSourceSubscriber = new OperatorSubscriber(
+      subscriber,
+      (value: T) => {
+        try {
+          if (predicate(value)) {
+            const branch = new Subject<T>();
+
+            branchSet.add(branch);
+
+            const branched = branch.asObservable();
+
+            subscriber.next(branched);
+          }
+
+          for (const branch of branchSet) {
+            branch.next(value);
+          }
+        } catch (err) {
+          handleError(err);
+        }
+      },
+      () => notify(consumer => consumer.complete()),
+      handleError,
+      () => branchSet.clear(),
+    );
+
+    source.subscribe(branchSourceSubscriber);
+  });
+}
---
+++ src/test/basic.test.ts
@@ -0,0 +1,39 @@
+import {firstValueFrom, mergeMap, range, take, toArray} from 'rxjs';
+
+import {branch} from '../library/index.js';
+
+test('basic', async () => {
+  const branches = await firstValueFrom(
+    range(0, 10).pipe(
+      branch(value => value % 2 === 0),
+      mergeMap(value$ => value$.pipe(toArray())),
+      toArray(),
+    ),
+  );
+
+  expect(branches).toEqual([
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [2, 3, 4, 5, 6, 7, 8, 9],
+    [4, 5, 6, 7, 8, 9],
+    [6, 7, 8, 9],
+    [8, 9],
+  ]);
+});
+
+test('take 3 from each branch', async () => {
+  const branches = await firstValueFrom(
+    range(0, 10).pipe(
+      branch(value => value % 2 === 0),
+      mergeMap(value$ => value$.pipe(take(3), toArray())),
+      toArray(),
+    ),
+  );
+
+  expect(branches).toEqual([
+    [0, 1, 2],
+    [2, 3, 4],
+    [4, 5, 6],
+    [6, 7, 8],
+    [8, 9],
+  ]);
+});
